af55a165a08bb7c2dae144a230a7d3bd86f73356
victor-s-salles <victor.salles@engenharia.ufjf.br>
2022-09-26 18:01:26 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -15,16 +15,41 @@ class App extends React.Component {
       cardRare: 'normal',
       cardTrunfo: false,
       hasTrunfo: false,
-      isSaveButtonDisabled: false,
+      isSaveButtonDisabled: true,
     };
   }
+
+  validateText = () => {
+    const { cardName, cardDescription, cardRare, cardImage } = this.state;
+    const testBoolText = (cardName && cardDescription && cardRare && cardImage);
+    return testBoolText;
+  };
+
+  validateAtt = () => {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const maxAtt = 90;
+    const maxTotal = 210;
+    const sunTotal = parseInt(cardAttr3, 10) + parseInt(cardAttr2, 10)
+    + parseInt(cardAttr1, 10);
+    const testBoolMax = (cardAttr1 > maxAtt || cardAttr2 > maxAtt || cardAttr3 > maxAtt);
+    const testBoolmin = (cardAttr1 < 0 || cardAttr2 < 0 || cardAttr3 < 0);
+    return !(testBoolMax || testBoolmin || sunTotal > maxTotal);
+  };
+
+  validadeGeneral = () => {
+    if (this.validateText() && this.validateAtt()) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  };
 
   handleChange = ({ target }) => {
     const { name } = target;
     const value = (target.type === 'checkbox') ? target.checked : target.value;
-    this.setState({
+    this.setState(() => ({
       [name]: value,
-    });
+    }), this.validadeGeneral);
   };
 
   onSaveButtonClick = () => 'a';
